5a2b99a34000cfd0a4e2b01aeb78efd2241063a2
Erdem Ayyıldız <ayyildizrdm@outlook.com>
2020-12-20 14:50:17 +0100

    First push

---
+++ SendtoSOMEAddIn/Ribbon1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace SendtoSOMEAddIn
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ribbon1));
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.HomeTabBtn = this.Factory.CreateRibbonButton();
+            this.tab2 = this.Factory.CreateRibbonTab();
+            this.group2 = this.Factory.CreateRibbonGroup();
+            this.ReadTabBtn = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.tab2.SuspendLayout();
+            this.group2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.ControlId.OfficeId = "TabMail";
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabMail";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.HomeTabBtn);
+            this.group1.Label = "Suspicious Mail";
+            this.group1.Name = "group1";
+            this.group1.Position = this.Factory.RibbonPosition.AfterOfficeId("GroupMailRespond");
+            // 
+            // HomeTabBtn
+            // 
+            this.HomeTabBtn.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.HomeTabBtn.Label = "Send to Cyber";
+            this.HomeTabBtn.Name = "HomeTabBtn";
+            this.HomeTabBtn.OfficeImageId = "ArrangeByImportance";
+            this.HomeTabBtn.ShowImage = true;
+            this.HomeTabBtn.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.HomeTabBtn_Click);
+            // 
+            // tab2
+            // 
+            this.tab2.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab2.ControlId.OfficeId = "TabReadMessage";
+            this.tab2.Groups.Add(this.group2);
+            this.tab2.Label = "TabReadMessage";
+            this.tab2.Name = "tab2";
+            // 
+            // group2
+            // 
+            this.group2.Items.Add(this.ReadTabBtn);
+            this.group2.Label = "Suspicious Mail";
+            this.group2.Name = "group2";
+            this.group2.Position = this.Factory.RibbonPosition.AfterOfficeId("GroupRespond");
+            // 
+            // ReadTabBtn
+            // 
+            this.ReadTabBtn.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.ReadTabBtn.Label = "Send to Cyber";
+            this.ReadTabBtn.Name = "ReadTabBtn";
+            this.ReadTabBtn.OfficeImageId = "ArrangeByImportance";
+            this.ReadTabBtn.ShowImage = true;
+            this.ReadTabBtn.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ReadTabBtn_Click);
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = resources.GetString("$this.RibbonType");
+            this.Tabs.Add(this.tab1);
+            this.Tabs.Add(this.tab2);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.tab2.ResumeLayout(false);
+            this.tab2.PerformLayout();
+            this.group2.ResumeLayout(false);
+            this.group2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton HomeTabBtn;
+        private Microsoft.Office.Tools.Ribbon.RibbonTab tab2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ReadTabBtn;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ SendtoSOMEAddIn/Ribbon1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace SendtoSOMEAddIn
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void HomeTabBtn_Click(object sender, RibbonControlEventArgs e)
+        {
+            try
+            {
+                DialogResult dialog = new DialogResult();
+                dialog = MessageBox.Show("Do you accept send selected mail to Cyber team?", "Send to Cyber", MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+                if (dialog == DialogResult.Yes)
+                {
+                    Outlook._Application _Application = new Outlook.Application();
+                    Outlook.MailItem selectedMail = new Microsoft.Office.Interop.Outlook.Application().ActiveExplorer().Selection[1];
+                    Outlook.MailItem mail = (Outlook.MailItem)_Application.CreateItem(Outlook.OlItemType.olMailItem);
+                    RegistryKey key = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Office\Outlook\Addins\SendtoSOMEAddIn");
+                    string Receipient = key.GetValue("ReceipientAddress").ToString();
+                    mail.To = Receipient;
+                    mail.Subject = "Suspicious Mail";
+                    mail.Body = "This mail seems like suspicious please investigate it. Regards";
+                    mail.Importance = Outlook.OlImportance.olImportanceNormal;
+                    mail.Attachments.Add(selectedMail, Outlook.OlAttachmentType.olByValue, 1, selectedMail.Subject);
+                    ((Outlook._MailItem)mail).Send();
+                    MessageBox.Show("Mail sent.", "succes", MessageBoxButtons.OK,MessageBoxIcon.Information);
+                    key.Close();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Please select a mail", "Send to Cyber",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            }
+        }
+
+        private void ReadTabBtn_Click(object sender, RibbonControlEventArgs e)
+        {
+            try
+            {
+                DialogResult dialog = new DialogResult();
+                dialog = MessageBox.Show("Do you accept send selected mail to Cyber team?", "Send to Cyber", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dialog == DialogResult.Yes)
+                {
+                    Outlook._Application _Application = new Outlook.Application();
+                    Outlook.MailItem selectedMail = new Microsoft.Office.Interop.Outlook.Application().ActiveExplorer().Selection[1];
+                    Outlook.MailItem mail = (Outlook.MailItem)_Application.CreateItem(Outlook.OlItemType.olMailItem);
+                    RegistryKey key = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Office\Outlook\Addins\SendtoSOMEAddIn");
+                    string Receipient = key.GetValue("ReceipientAddress").ToString();
+                    mail.To = Receipient;
+                    mail.Subject = "Suspicious Mail";
+                    mail.Body = "This mail seems like suspicious please investigate it. Regards";
+                    mail.Importance = Outlook.OlImportance.olImportanceNormal;
+                    mail.Attachments.Add(selectedMail, Outlook.OlAttachmentType.olByValue, 1, selectedMail.Subject);
+                    ((Outlook._MailItem)mail).Send();
+                    MessageBox.Show("Mail sent.", "succes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    key.Close();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Please select a mail", "Send to Cyber", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
